94a3fa7d6a76ad869246709496b7eeadb5890aa7
LeDao <1203007469@qq.com>
2020-10-11 01:03:46 +0800

    LeDao的博客

--- src/main/java/com/ledao/ErrorPageConfig.java
+++ src/main/java/com/ledao/ErrorPageConfig.java
@@ -1,27 +1,25 @@
 package com.ledao;
 
-import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 
 /**
  * @author LeDao
  * @company
- * @create 2020-10-10 23:03
+ * @create 2020-10-11 00:27
  */
 @Configuration
-public class ErrorPageConfig {
+public class ErrorPageConfig implements ErrorPageRegistrar {
 
-    @Bean
-    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
-        //第二种写法：java8 lambda写法
-        return (factory -> {
-            ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-            factory.addErrorPages(errorPage404);
-        });
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        ErrorPage[] errorPages = new ErrorPage[2];
+        errorPages[0] = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+        errorPages[1] = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
 
+        registry.addErrorPages(errorPages);
     }
 }
